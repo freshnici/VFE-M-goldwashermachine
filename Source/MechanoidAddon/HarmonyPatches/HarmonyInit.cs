38058ba65c0debfce95d81b40678a34759db507e
freshnici <35426764+freshnici@users.noreply.github.com>
2021-12-02 20:06:12 +0100

    Initial commit

---
+++ Source/MechanoidAddon/HarmonyPatches/HarmonyInit.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace MechanoidAddon
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyInit
+    {
+        static HarmonyInit()
+        {
+            new Harmony("OskarPotocki.VFEMechanoidsAddons").PatchAll();
+        }
+    }
+}
